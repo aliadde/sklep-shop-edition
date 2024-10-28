4231b08a3f4402e3f248d5fd665788da0a220db3
ali <aamrt2024@gmail.com>
2024-10-28 07:56:01 +0000

    more res

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import "./App.css";
 import "./assets/media768.css";
 import "./assets/media578.css";
+import "./assets/media425.css";
+import "./assets/media1024.css";
 function App() {
   return (
     <>
